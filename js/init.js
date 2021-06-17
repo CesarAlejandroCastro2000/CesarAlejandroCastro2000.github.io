59103743d627d2683b54d3249842568dbfebf3f2
CesarAlejandroCastro2000 <cesarcastroreyes2000@gmail.com>
2021-06-16 22:05:55 -0500

    Api 2

--- js/init.js
+++ js/init.js
@@ -9,10 +9,10 @@
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyAUZ5r8_cZzpbiGinX5glHi9nwHN9SKYwQ",
-    authDomain: "hospitalwebsystem32.firebaseapp.com",
-    projectId: "hospitalwebsystem32",
-    storageBucket: "hospitalwebsystem32.appspot.com",
-    messagingSenderId: "842522904363",
-    appId: "1:842522904363:web:a39b5870b6d4879608e76e",
-    measurementId: "G-S6CERMPDD3"
+  authDomain: "hospitalwebsystem32.firebaseapp.com",
+  projectId: "hospitalwebsystem32",
+  storageBucket: "hospitalwebsystem32.appspot.com",
+  messagingSenderId: "842522904363",
+  appId: "1:842522904363:web:a39b5870b6d4879608e76e",
+  measurementId: "G-S6CERMPDD3"
 });
